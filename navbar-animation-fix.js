834fe7b5563f7173ae3794c3ffc7fa4a66ccdc01
Alefros <alef.r.oliveira@gmail.com>
2021-10-07 00:33:44 -0300

    Correção de posicionamento pós abertura do menu

--- navbar-animation-fix.js
+++ navbar-animation-fix.js
@@ -1,7 +1,7 @@
 $('#navbar-topCasaFina').on('show.bs.collapse', function() {
-    $('.topCasaFina-banner').css('transform', 'translate(-50%, 10%)');
+    $('.topCasaFina-banner').css('transform', 'translate(-40%, 10%)');
 });
 
 $('#navbar-topCasaFina').on('hide.bs.collapse', function() {
-    $('.topCasaFina-banner').css('transform', 'translate(-50%, -50%)');
+    $('.topCasaFina-banner').css('transform', 'translate(-40%, -50%)');
 });
